27389e7758a2d0c6967dfeb4c3333f0a5f7f3a3c
100490578 <ikergaraujo@gmail.com>
2026-02-19 22:02:08 +0100

    Fix logic errors on iteration for poping and duplicate aquisition

--- crtsh_domain_enum.py
+++ crtsh_domain_enum.py
@@ -4,7 +4,9 @@ import re
 def get_html(domain):
 	query = "https://crt.sh/?q="+domain+"&exclude=expired"
 	response = requests.get(query)
-	print(f"Status: {response.status_code}")		
+	print(f"Status: {response.status_code}")	
+	if response.status_code!= 200:
+			return "check the status"	
 	response = response.text
 	pattern = rf'\S+{re.escape(domain)}\S+'
 	matching_lines = re.findall(pattern, response, re.IGNORECASE)
@@ -12,12 +14,16 @@ def get_html(domain):
 	domains = []
 	for line in matching_lines:
 		domains += re.findall(clearning_lines,line,re.IGNORECASE)    
-	#getting rid of duplicates	
-	j =1
-	for i in range(len(domains)-1):		
-		for j in range(len(domains)-2):
+	#getting rid of duplicates		
+	print("deleting duplicates...")
+	duplicates = []
+	for i in range(0,len(domains)-1,1):		
+		for j in range(1,len(domains),1):
 			if domains[i] == domains[j]:
-				domains.pop(j)
+				duplicates.append(i) 
+	
+	for j in sorted(duplicates, reverse=True):
+		domains.pop(j)
 	print(f"Found {len(domains)} matches")
 	domains = "\n".join(domains)
 	return domains  
